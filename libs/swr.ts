0f2e55358589d1076bdef5e8896c69afd1745bf9
sehandev <sehandev@gmail.com>
2023-09-16 17:56:20 +0000

    EDIT Update SWRManager

--- libs/swr.ts
+++ libs/swr.ts
@@ -7,16 +7,30 @@ let swrManager: SWRManager
 export class SWRManager {
   fetcher: Fetcher<any, string>
   logger: Logger
+  externURI: string
 
   constructor() {
     this.logger = getLogger()
     this.logger.log('SWRManager', 'constructor')
     this.fetcher = (...args) => fetch(...args).then((res) => res.json())
+    this.externURI = 'http://admin-api.aboutmeeting.co.kr'
+  }
+
+  log(message: string, data?: any): void {
+    this.logger.log('SWRManager', message, data)
   }
 
   getFetcher() {
-    this.logger.log('SWRManager', 'getFetcher')
+    this.log('getFetcher')
     return this.fetcher
+  }
+
+  convertAPI(uri: string): string {
+    this.log('convertAPI', uri)
+    if (uri.charAt(0) === '/') {
+      uri = uri.slice(1)
+    }
+    return `${this.externURI}/${uri}`
   }
 }
 
