db20fe3566df4d848ffb7b255e492c5e7373802f
sehandev <sehandev@gmail.com>
2023-09-16 18:00:39 +0000

    EDIT Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,13 @@
-import useSWR from 'swr'
-
-import { Article } from '@api/blog'
 import { ThemeToggleButton } from '@components/theme-button'
 import { getLogger } from '@libs/logger'
-import { getSWRManager } from '@libs/swr'
 
 export default function Home() {
   const logger = getLogger()
-  const swrManager = getSWRManager()
-
-  const { data, isLoading } = useSWR<Article, Error>('/api/blog', swrManager.getFetcher())
+  logger.log('Home', `visit`)
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <>
-          <h1 className="">{data?.title}</h1>
-          <div>{data?.date}</div>
-        </>
-      )}
-      <button
-        className="btn btn-primary"
-        onClick={() => {
-          logger.log('Home', 'test', data)
-        }}
-      >
-        테스트 버튼
-      </button>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="font-semibold text-2xl mb-4">어바웃미팅 관리</h1>
       <ThemeToggleButton />
     </main>
   )
