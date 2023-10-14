6f833b19cd9c6c43521063b6fbd59f9cde77802a
sehandev <sehandev@gmail.com>
2023-10-14 15:54:45 +0900

    Module apply

--- pages/reservation/cafe/[name].tsx
+++ pages/reservation/cafe/[name].tsx
@@ -1,10 +1,6 @@
 import useSWR from 'swr'
 
-import { SMSLink } from '@components/link'
-import { Loading } from '@components/reservation/loading'
-import { ReservationTableHeaderRow, ReservationTableRow } from '@components/reservation/table'
-import { ThemeToggleButton } from '@components/theme-button'
-import { Table, TableBody, TableCaption, TableHeader } from '@components/ui/table'
+import { ReservationMain } from '@components/reservation/main'
 import { getLogger } from '@libs/logger'
 import { getSWRManager } from '@libs/swr'
 import { Reservation } from '@type/reservation'
@@ -12,7 +8,7 @@ import { Reservation } from '@type/reservation'
 const SAMPLE_CS_PHONE: string = '010-5397-8929'
 const SAMPLE_CAFE_NAME: string = 'CAFE_OGO'
 
-export default function CafeReservation() {
+export default function DarkReservation() {
   const logger = getLogger()
   const swrManager = getSWRManager()
   logger.log('CafeReservation', `visit`, { cafe_name: SAMPLE_CAFE_NAME })
@@ -22,39 +18,11 @@ export default function CafeReservation() {
   )
 
   return (
-    <main className="flex flex-col items-center justify-start min-h-screen sm:p-24 p-4">
-      <div className="flex flex-row items-center justify-center mb-4 gap-4">
-        <ThemeToggleButton />
-        <h1 className="font-semibold text-2xl">{SAMPLE_CAFE_NAME}</h1>
-      </div>
-      {isLoading ? (
-        <Loading cs_phone={SAMPLE_CS_PHONE} />
-      ) : (
-        <>{reservationArray && <ReservationTable reservationArray={reservationArray} />}</>
-      )}
-    </main>
-  )
-}
-
-const ReservationTable = ({ reservationArray }: { reservationArray: Reservation[] }) => {
-  function sortReservationArrayByTimestamp(a: Reservation, b: Reservation): number {
-    if (a.timestamp < b.timestamp) return -1
-    return 1
-  }
-
-  return (
-    <Table className="mx-auto min-w-max w-fit">
-      <TableCaption className="my-4">
-        문의는 <SMSLink>{SAMPLE_CS_PHONE}</SMSLink>로 문자 남겨주세요.
-      </TableCaption>
-      <TableHeader>
-        <ReservationTableHeaderRow />
-      </TableHeader>
-      <TableBody>
-        {reservationArray.sort(sortReservationArrayByTimestamp).map((info, idx) => {
-          return <ReservationTableRow key={`reservation-${idx}`} info={info} />
-        })}
-      </TableBody>
-    </Table>
+    <ReservationMain
+      csPhone={SAMPLE_CS_PHONE}
+      reservationArray={reservationArray}
+      locationName={SAMPLE_CAFE_NAME}
+      isLoading={isLoading}
+    />
   )
 }
--- pages/reservation/dark/[name].tsx
+++ pages/reservation/dark/[name].tsx
@@ -1,10 +1,6 @@
 import useSWR from 'swr'
 
-import { SMSLink } from '@components/link'
-import { Loading } from '@components/reservation/loading'
-import { ReservationTableHeaderRow, ReservationTableRow } from '@components/reservation/table'
-import { ThemeToggleButton } from '@components/theme-button'
-import { Table, TableBody, TableCaption, TableHeader } from '@components/ui/table'
+import { ReservationMain } from '@components/reservation/main'
 import { getLogger } from '@libs/logger'
 import { getSWRManager } from '@libs/swr'
 import { Reservation } from '@type/reservation'
@@ -22,39 +18,11 @@ export default function DarkReservation() {
   )
 
   return (
-    <main className="flex flex-col items-center justify-start min-h-screen sm:p-24 p-4">
-      <div className="flex flex-row items-center justify-center mb-4 gap-4">
-        <ThemeToggleButton />
-        <h1 className="font-semibold text-2xl">{SAMPLE_DARK_NAME}</h1>
-      </div>
-      {isLoading ? (
-        <Loading cs_phone={SAMPLE_CS_PHONE} />
-      ) : (
-        <>{reservationArray && <ReservationTable reservationArray={reservationArray} />}</>
-      )}
-    </main>
-  )
-}
-
-const ReservationTable = ({ reservationArray }: { reservationArray: Reservation[] }) => {
-  function sortReservationArrayByTimestamp(a: Reservation, b: Reservation): number {
-    if (a.timestamp < b.timestamp) return -1
-    return 1
-  }
-
-  return (
-    <Table className="mx-auto min-w-max w-fit">
-      <TableCaption className="my-4">
-        문의는 <SMSLink>{SAMPLE_CS_PHONE}</SMSLink>로 문자 남겨주세요.
-      </TableCaption>
-      <TableHeader>
-        <ReservationTableHeaderRow />
-      </TableHeader>
-      <TableBody>
-        {reservationArray.sort(sortReservationArrayByTimestamp).map((info, idx) => {
-          return <ReservationTableRow key={`reservation-${idx}`} info={info} />
-        })}
-      </TableBody>
-    </Table>
+    <ReservationMain
+      csPhone={SAMPLE_CS_PHONE}
+      reservationArray={reservationArray}
+      locationName={SAMPLE_DARK_NAME}
+      isLoading={isLoading}
+    />
   )
 }
